cb5407d8d383ce71f6a61da91a821c2c4a05d310
Isaac Gomes <isaac.go13@hotmail.com>
2018-04-22 20:09:06 -0300

    docs(Retangulo): documentação da classe Retangulo

--- Tratamento_de_Poligonos/retangulo.h
+++ Tratamento_de_Poligonos/retangulo.h
@@ -2,9 +2,19 @@
 #define RETANGULO_H
 #include "poligono.h"
 
+/**
+ * @brief A classe Retangulo é uma subclasse da classe Poligono que representa retângulos.
+ */
 class Retangulo : public Poligono
 {
 public:
+    /**
+     * @brief Retangulo é o construtor da classe Retangulo
+     * @param x é a coordenada x do canto superior esquerdo (Ponto Superior Esquerdo) do Retangulo
+     * @param y é a coordenada y do canto superior esquerdo (Ponto Superior Esquerdo) do Retangulo
+     * @param largura é a largura a ser definida pro Retangulo
+     * @param altura é a altura a ser definida pro Retangulo
+     */
     Retangulo(float x, float y, float largura, float altura);
 };
 
