097279c8de98b1d944677e71c63b4fdb4e324922
Isaac Gomes <isaac.go13@hotmail.com>
2018-04-24 23:51:26 -0300

    fix(#2): corrige cálculo para Retangulo

--- Tratamento_de_Poligonos/retangulo.cpp
+++ Tratamento_de_Poligonos/retangulo.cpp
@@ -6,5 +6,5 @@ Retangulo::Retangulo(float x, float y, float largura, float altura)
     setVetPol(3,x,y);
     setVetPol(2,x+largura,y);
     setVetPol(1,x+largura,y-altura);
-    setVetPol(0,x,y-largura);
+    setVetPol(0,x,y-altura);
 }
