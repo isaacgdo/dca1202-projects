9cd045483c81b589fc1943b7c8a35686be5ffed2
Isaac Gomes de Oliveira <isaac.go13@hotmail.com>
2018-05-11 15:40:09 -0300

    clear code

--- Tratamento_Classes_Abstratas/reta.cpp
+++ Tratamento_Classes_Abstratas/reta.cpp
@@ -22,15 +22,16 @@ void Reta::draw(Screen &t)
     int Delta_y = abs(yf - yi);
     int s1 = Sinal(xf - xi);
     int s2 = Sinal(yf - yi);
+    
     if(Delta_y > Delta_x){
         int Temp = Delta_x;
         Delta_x = Delta_y;
         Delta_y = Temp;
         Troca = 1;
-    }
-    else{
+    } else{
         Troca = 0;
     }
+    
     int new_e = 2*Delta_y - Delta_x;
     for(int i = 1; i <= Delta_x; i++){
         t.setPixel(x,y);
@@ -38,8 +39,7 @@ void Reta::draw(Screen &t)
             if(Troca == 1){
                 //Muda para a proxima linha de rasterização
                 x = x + s1;
-            }
-            else{
+            } else{
                 y = y + s2;
             }
             new_e = new_e - 2*Delta_x;
@@ -48,8 +48,7 @@ void Reta::draw(Screen &t)
         //Permanece nesta linha de rasterização
         if(Troca == 1){
             y = y + s2;
-        }
-        else{
+        } else{
             x = x + s1;
         }
         new_e = new_e + 2*Delta_y;
@@ -60,5 +59,5 @@ int Reta::Sinal(int x)
 {
     if(x < 0){ return -1; }
     if(x == 0){ return 0; }
-    else{ return 1; }
+    return 1; 
 }
