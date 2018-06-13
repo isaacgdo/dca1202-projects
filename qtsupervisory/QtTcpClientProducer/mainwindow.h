f71f3c2df6494b5c7f6ae4578eb4b7084ad78267
Isaac Gomes <isaac.go13@hotmail.com>
2018-06-12 21:39:08 -0300

    docs: runned doxygen

--- qtsupervisory/QtTcpClientProducer/mainwindow.h
+++ qtsupervisory/QtTcpClientProducer/mainwindow.h
@@ -7,9 +7,13 @@
 #include <QString>
 
 namespace Ui {
+
 class MainWindow;
 }
 
+/**
+ * @brief A classe MainWindow representa o contâiner da janela da aplicação e seus componentes
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -23,16 +27,30 @@ public slots:
      * @brief putData é um função que cria um temporalizador usado na função timerEvent para possibilitar o envio constante de dados ao servidor.
      */
     void putData();
+
     /**
      * @brief stopData é um função que "mata" o temporalizador criado pela função putData , fazendo com que o envio de dados ao servidor cesse.
      */
     void stopData();
+
     /**
      * @brief getIP é um função que grava o conteudo pressente no campo de texto do objeto lineEdit em uma Qstring.
      */
     QString getIP();
+
+    /**
+     * @brief tcpConnect abre uma conexão com um servidor informado
+     */
     void tcpConnect();
+
+    /**
+     * @brief tcpDisconnect fecha uma conexão com um servidor informado
+     */
     void tcpDisconnect();
+
+    /**
+     * @brief timerEvent seta valores temporais a serem enviados pro servidor
+     */
     void timerEvent(QTimerEvent *event);
 
 private:
